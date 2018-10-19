d0bcd849e232ae6fd743a8ddd6730a0e7107da72
nikhil-muskowl <nikhil.muskowl@gmail.com>
2018-10-19 16:58:53 +0530

    update

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var jQuery: any;
+declare var $: any;
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -10,6 +11,16 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    jQuery('.about-us-carousel').owlCarousel({
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      margin:30,
+      nav:true,
+      navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+      items:1,
+      dots: false,
+    });
   }
 
 }
